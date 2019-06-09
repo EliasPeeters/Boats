7b27afd33e96c871f0fad9660d2112dd398984cb
Elias <elias.peeters@icloud.com>
2019-06-09 12:42:13 +0200

    Added Handler

---
+++ src/com/elias/boats/Handler.java
@@ -0,0 +1,15 @@
+package com.elias.boats;
+
+import com.elias.boats.worlds.World;
+
+public class Handler {
+
+    private Game game;
+    private World world;
+
+    public Handler(Game game) {
+
+    }
+
+
+}
