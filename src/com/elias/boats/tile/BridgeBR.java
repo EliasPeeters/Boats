ec1bbb44b45750e8e037ecfee9f2625efb237a55
PavelMaschke <42940576+PavelMaschke@users.noreply.github.com>
2019-06-11 19:08:22 +0200

    Edited Paths

---
+++ src/com/elias/boats/tile/BridgeBR.java
@@ -0,0 +1,13 @@
+package com.elias.boats.tile;
+
+import com.elias.boats.Assets;
+
+public class BridgeBR extends Tile {
+
+    public BridgeBR(int id) {
+        super(Assets.bridgeBR, id);
+    }
+
+
+
+}
---
+++ src/com/elias/boats/tile/DirtCBL.java
@@ -0,0 +1,13 @@
+package com.elias.boats.tile;
+
+import com.elias.boats.Assets;
+
+public class DirtCBL extends Tile {
+
+    public DirtCBL(int id) {
+        super(Assets.dirtCBL, id);
+    }
+
+
+
+}
