39768cf12b15edc5dca56e9c7e6c59d8cb623b96
Elias <elias.peeters@icloud.com>
2019-06-11 14:06:36 +0200

    Changed cursor

--- src/com/elias/boats/Launcher.java
+++ src/com/elias/boats/Launcher.java
@@ -1,5 +1,8 @@
 package com.elias.boats;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Launcher {
     public static void main(String[] args) {
         Game game = new Game("Rush Hour", 1000, 600);
